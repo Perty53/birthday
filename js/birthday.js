1f2234a87da4f6e8f0daa9cdce91544cbd482e77
Perty53 <alexandrepertiny693@gmail.com>
2023-03-07 19:22:42 -0500

    ---

--- js/birthday.js
+++ js/birthday.js
@@ -1,5 +1,14 @@
 window.onload = ()=>{
+    let wish_btn = $('#wish-btn');
+    
+    wish_btn.on('click', (e) =>{
+        let init_content = $('#initial-content');
+
+        init_content.animate({
+            top: "-=500"
+        }, 5000)
+    });
+
     
 }
-
 
