3731550e4a881b2d05a403fba7cc9f1e1c63dc20
Perty53 <alexandrepertiny693@gmail.com>
2023-03-13 16:37:47 -0400

    --

--- js/birthday.js
+++ js/birthday.js
@@ -2,13 +2,25 @@ window.onload = ()=>{
     let wish_btn = $('#wish-btn');
     
     wish_btn.on('click', (e) =>{
-        let init_content = $('#initial-content');
-
+        wish_btn.toggle();
+        let init_content = $('#initial-content'),
+         main_content = $('#main-content'),
+         main_height = main_content.height(),
+        init_height = init_content.height()
+        newPos = init_height;
+        console.log(init_content.html())
         init_content.animate({
-            top: "-=500"
-        }, 5000)
-    });
-
-    
+            top: newPos
+        }, 1200, (e) =>{addNewContent()})
+    });    
 }
 
+function addNewContent(){
+    let birth_form = $('#wishes-form'),
+    nameLabel = $('<label for ="name">Saisir le nom de la personne :</label>')
+    ;
+
+    birth_form.addClass("formAdjust");
+    birth_form.append(nameLabel)
+    
+}
