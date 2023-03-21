95ea6acdf5203f04c67ad6b94095e43de7d2229f
Perty53 <alexandrepertiny693@gmail.com>
2023-03-21 16:22:13 -0400

    script

--- js/birthday.js
+++ js/birthday.js
@@ -1,24 +1,63 @@
-window.onload = ()=>{
+window.onload = () => {
     let wish_btn = $('#wish-btn');
-    
-    wish_btn.on('click', (e) =>{
+
+    wish_btn.on('click', (e) => {
         wish_btn.toggle();
         let init_content = $('#initial-content'),
-         main_content = $('#main-content'),
-         main_height = main_content.height(),
-        init_height = init_content.height()
+            main_content = $('#main-content'),
+            main_height = main_content.height(),
+            init_height = init_content.height()
         newPos = init_height;
         console.log(init_content.html())
         init_content.animate({
             top: newPos
-        }, 1200, (e) =>{addNewContent()})
-    });    
+        }, 1200, (e) => { addNewContent() })
+    });
+
+
 }
 
-function addNewContent(){
+function addNewContent() {
     let formContainer = $('#form-container');
-    formContainer.append(fetch('../assets/form.html').then(resp => resp.text()).then(rep => formContainer.append(rep)));
-   
-    formContainer.addClass("formAdjust");
-      
+    formContainer.append(fetch('../assets/form.html').then(resp => resp.text()).then(rep => {
+        formContainer.append(rep);
+        formContainer.addClass("formAdjust");
+
+
+
+        let month = $('#birth-month'),
+            day = $("#birth-day"),
+            year = $("#birth-year"),
+            months = ['Janvier', "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+            days = (n) => {
+                for (let i = 1; i <= n; i++) {
+                    day.html(day.html() + "<option>" + i + "</option>");
+                }
+            };
+
+        for (let m = 0; m < months.length; m++) {
+            month.html(month.html() + "<option>" + months[m] + "</option>");
+        }
+
+        if (month === "Fevrier") {
+            day(28);
+        } else if (month === "Avril" || month === "Juin" || month === "Septembre" || month === "Novembre") {
+            days(30);
+        } else {
+            days(31);
+        }
+
+        for (let as = 1950; as <= 2099; as++) {
+            year.html(year.html() + '<option>' + as + '</option>');
+
+            if (as % 400 === 0 || as % 100 !== 0 && as % 4 == 0) {
+
+            }
+
+        }
+
+    }));
+
+
 }
+
